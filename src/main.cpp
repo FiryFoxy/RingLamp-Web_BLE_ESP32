41f1af4ce0f741273d80fe56f944e006d33adb63
FiryFoxy <86464200+FiryFoxy@users.noreply.github.com>
2025-01-10 17:02:07 +0100

    working!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,36 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <Adafruit_NeoPixel.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pLedCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+#define SERVICE_UUID "19b10000-e8f2-537e-4f6c-d104768a1214"
+#define LED_CHARACTERISTIC_UUID "19b10002-e8f2-537e-4f6c-d104768a1214"
 
 #define LED_PIN1 15
 #define LED_PIN2 16
 #define LED_COUNT 12
-
 Adafruit_NeoPixel strip1(LED_COUNT, LED_PIN1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip2(LED_COUNT, LED_PIN2, NEO_GRB + NEO_KHZ800);
 
-BLEServer* pServer = NULL;
-BLECharacteristic* pLedControlCharacteristic = NULL;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-
-// UUIDs for BLE Service and Characteristics
-#define SERVICE_UUID "19b10000-e8f2-537e-4f6c-d104768a1214"
-#define LED_CONTROL_CHARACTERISTIC_UUID "19b10002-e8f2-537e-4f6c-d104768a1214"
-
-// Global variables for LED control
 bool isOn = false;
-uint32_t currentColor = strip1.Color(255, 255, 255); // Default color: white
+uint32_t currentColor1 = strip1.Color(255, 255, 255);
+uint32_t currentColor2 = strip2.Color(255, 255, 255);
 
 void updateLEDs() {
+  Serial.println(isOn ? "Updating LEDs: ON" : "Updating LEDs: OFF");
   if (isOn) {
-    for (int i = 0; i < LED_COUNT; i++) {
-      strip1.setPixelColor(i, currentColor);
-      strip2.setPixelColor(i, currentColor);
+    for (int i = 0; i < strip1.numPixels(); i++) {
+    strip1.setPixelColor(i, currentColor1);
+    }
+    for (int i = 0; i < strip2.numPixels(); i++) {
+    strip2.setPixelColor(i, currentColor2);
     }
     strip1.show();
     strip2.show();
@@ -41,26 +42,28 @@ void updateLEDs() {
   }
 }
 
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
-  }
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        deviceConnected = true;
+        Serial.println("Client connected");
+    }
 
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
-  }
+    void onDisconnect(BLEServer* pServer) {
+        deviceConnected = false;
+        Serial.println("Client disconnected");
+    }
 };
 
 class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic* pCharacteristic) {
     std::string rawValue = pCharacteristic->getValue();
     if (rawValue.length() > 0) {
-      char command = rawValue[0];
-      if (command == '0') { // Turn off
+      uint8_t command = rawValue[0];
+      if (command == 0) { // Turn off
         isOn = false;
         updateLEDs();
         Serial.println("LEDs turned off");
-      } else if (command == '1') { // Turn on
+      } else if (command == 1) { // Turn on
         isOn = true;
         updateLEDs();
         Serial.println("LEDs turned on");
@@ -69,7 +72,8 @@ class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
           uint8_t r = rawValue[1];
           uint8_t g = rawValue[2];
           uint8_t b = rawValue[3];
-          currentColor = strip1.Color(r, g, b);
+          currentColor1 = strip1.Color(r, g, b);
+          currentColor2 = strip2.Color(r, g, b);
           updateLEDs();
           Serial.printf("Color changed to R:%d G:%d B:%d\n", r, g, b);
         }
@@ -79,59 +83,49 @@ class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
 };
 
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
+    Serial.println("Initializing BLE...");
 
-  // Initialize LED strips
-  strip1.begin();
-  strip1.show();
-  strip1.setBrightness(50);
+    strip1.begin();
+    strip1.show();
+    strip1.setBrightness(50);
 
-  strip2.begin();
-  strip2.show();
-  strip2.setBrightness(50);
+    strip2.begin();
+    strip2.show();
+    strip2.setBrightness(50);
 
-  // Create the BLE Device
-  BLEDevice::init("ESP32 Lamp");
+    BLEDevice::init("ESP32-Lamp");
 
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
-  BLEService* pService = pServer->createService(SERVICE_UUID);
+    BLEService* pService = pServer->createService(SERVICE_UUID);
 
-  // Create the LED Control Characteristic
-  pLedControlCharacteristic = pService->createCharacteristic(
-    LED_CONTROL_CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_WRITE
-  );
-  pLedControlCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
-  pLedControlCharacteristic->addDescriptor(new BLE2902());
+    pLedCharacteristic = pService->createCharacteristic(
+        LED_CHARACTERISTIC_UUID,
+        BLECharacteristic::PROPERTY_WRITE
+    );
 
-  // Start the service
-  pService->start();
+    pLedCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+    pLedCharacteristic->addDescriptor(new BLE2902());
 
-  // Start advertising
-  BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);
-  BLEDevice::startAdvertising();
-  Serial.println("Waiting for a client connection...");
+    pService->start();
+
+    BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->start();
+
+    Serial.println("BLE initialized. Waiting for connections...");
 }
 
 void loop() {
-  // Handle connection state changes
-  if (!deviceConnected && oldDeviceConnected) {
-    Serial.println("Device disconnected.");
-    delay(500);
-    pServer->startAdvertising();
-    Serial.println("Restart advertising.");
-    oldDeviceConnected = deviceConnected;
-  }
-
-  if (deviceConnected && !oldDeviceConnected) {
-    Serial.println("Device connected.");
-    oldDeviceConnected = deviceConnected;
-  }
+    if (deviceConnected && !oldDeviceConnected) {
+        Serial.println("Device connected for the first time");
+        oldDeviceConnected = deviceConnected;
+    } else if (!deviceConnected && oldDeviceConnected) {
+        Serial.println("Device disconnected, restarting advertising");
+        oldDeviceConnected = deviceConnected;
+        delay(500);
+        pServer->startAdvertising();
+    }
 }
